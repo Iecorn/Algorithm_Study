564022a15b84467b0ffc724fb7c18ef57f75f8c3
ECON <lgyoon621@naver.com>
2024-02-04 18:35:53 +0900

    Initial commit

---
+++ src/main/java/org/example/LV1/Knight_Weapon.java
@@ -0,0 +1,38 @@
+package org.example.LV1;
+
+public class Knight_Weapon {
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+
+        for (int i = 1 ; i <= number ; i++){
+
+            int damage = countDivisors(i);
+
+            if (damage > limit)
+                answer += power;
+            else
+                answer += damage;
+        }
+
+
+        return answer;
+    }
+
+    static int countDivisors(int n)
+    {
+        int cnt = 0;
+        for (int i = 1; i <= Math.sqrt(n); i++)
+        {
+            if (n % i == 0) {
+
+                if (n / i == i)
+                    cnt++;
+
+                else
+                    cnt = cnt + 2;
+            }
+        }
+        return cnt;
+
+    }
+}
---
+++ src/main/java/org/example/LV1/Running_Race.java
@@ -0,0 +1,26 @@
+package org.spirng.LV1;
+
+import java.util.HashMap;
+
+public class Running_Race {
+    public String[] solution(String[] players, String[] callings) {
+        String[] answer = new String[players.length];
+        HashMap<String, Integer> player_map = new HashMap<>();
+
+        for(int i = 1; i <= players.length; i++)
+            player_map.put(players[i-1], i);
+
+        for (String calling : callings) {
+            int value = player_map.get(calling);
+            String forward = players[value - 2];
+
+            player_map.put(calling, value-1);
+            player_map.put(forward, value);
+
+            players[value-2] = players[value-1];
+            players[value-1]=forward;
+        }
+
+        return players;
+    }
+}
